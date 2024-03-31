2f71e8b843deafcf070f40013d5d103266ca0c36
Phong <thanhphong07022002@gmail.com>
2024-03-31 11:24:19 +0700

    refactor

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  return res.status(200).json({ Message: 'Nothing here!' });
-});
-
-module.exports = router;
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  return res.status(200).json({"Message":"Nothing here!"})
-});
-
-module.exports = router;
