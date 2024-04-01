9f5fbef447ca34a0e8c9efda6d3c72c4698cff44
PhamDat328 <92577783+PhamDat328@users.noreply.github.com>
2024-04-02 00:32:43 +0700

    style product with carousel, filter product by gender, add footer and more

--- client/src/components/Products/ProductList.tsx
+++ client/src/components/Products/ProductList.tsx
@@ -1,44 +1,83 @@
 import { useEffect, useState } from "react";
 import { Product } from "../../types/Product";
 import ProductItem from "./ProductItem";
+import Slider from "react-slick";
 
 const ProductList = () => {
   const [products, setProducts] = useState<Product[] | []>([]);
+  const [gender, setGender] = useState<string>("Male");
+  const category: Array<{ [key: string]: string }> = [
+    { Man: "Male" },
+    { Women: "Female" },
+    { Unisex: "Unisex" },
+  ];
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+  };
+
+  async function fetchData(query: string) {
+    await fetch(`http://localhost:8080/products/1?gender=${query}`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products));
+  }
   useEffect(() => {
-    async function fetchData() {
-      await fetch("http://localhost:8080/products/1")
-        .then((res) => res.json())
-        .then((data) => setProducts(data.products));
-    }
-
-    fetchData();
-  }, []);
+    fetchData(gender!);
+  }, [gender]);
   console.log(products);
 
   return (
-    <div className="my-20 w-full">
+    <div className="mb-12 mt-20 w-full">
       <div className="mx-auto flex w-[80%] flex-col items-center">
         <h1 className="text-4xl">BEST SELLERS PRODUCTS</h1>
-        <p>The stylish and organized cosmetic products</p>
+        <p className="m-2 text-xl opacity-40">
+          The stylish and organized cosmetic products
+        </p>
         <div className="m-4 flex cursor-pointer gap-4 text-xl">
-          <div className="group">
-            <p>Man</p>
-            <hr className="border border-white transition ease-in-out group-hover:w-full group-hover:border-black group-hover:transition-all group-hover:duration-100" />
-          </div>
-          <p>Women</p>
-          <p>Unisex</p>
+          {category.map((obj, index) => {
+            const item = Object.entries(obj);
+            return (
+              <p
+                className={
+                  gender == item[0][1] ? "text-[#f8b500]" : "opacity-[0.3]"
+                }
+                key={index}
+                onClick={() => setGender(item[0][1])}
+              >
+                {item[0][0]}
+              </p>
+            );
+          })}
         </div>
       </div>
-      <div className="mx-auto grid w-[1280px] grid-cols-5 gap-y-4">
+      <Slider
+        className="h-[420px] overflow-x-hidden overflow-y-hidden"
+        {...settings}
+      >
         {products.map((product) => (
-          <div key={product.PID}>
+          <div key={product.PID} className="h-[380px]">
             <ProductItem product={product} />
           </div>
         ))}
-      </div>
+      </Slider>
     </div>
   );
 };
 
 export default ProductList;
+
+{
+  /* <div className="group">
+            <p
+              onClick={() => setGender("Male")}
+              className="relative inline-block text-[#0087ca] after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:transform after:bg-[#0087ca] after:transition after:delay-[250ms] after:ease-out after:content-[''] hover:after:origin-bottom-left hover:after:scale-x-[1] aria-[current=page]:font-medium aria-[current=page]:text-[#f8b500]"
+            >
+              Man
+            </p>
+          </div>
+           */
+}
